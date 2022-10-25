5e050372cc84399f4d5334435b95add1ad8f1bdc
a10n2 <yankit.alone@gmail.com>
2022-10-25 15:08:37 +0800

    Create clear-screen.js

---
+++ node/clear-screen.js
@@ -0,0 +1,11 @@
+/*
+* 清理终端信息，保留上次记录
+* via vitejs
+*/
+function clearScreen() {
+  const repeatCount = process.stdout.rows - 2;
+  const blank = repeatCount > 0 ? "\n".repeat(repeatCount) : "";
+  console.log(blank);
+  readline.cursorTo(process.stdout, 0, 0);
+  readline.clearScreenDown(process.stdout);
+}
